e9a2239f50dcf7c308a7e746cd333a03250fa32c
Mike Treit <mtreit@ntdev.microsoft.com>
2025-02-26 12:00:33 -0800

    Initial custom build task example

---
+++ src/CustomBuildTask/BuildTask.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Test
+{
+    public class CustomBuildTask : Task
+    {
+        public override bool Execute()
+        {
+            Log.LogMessage(MessageImportance.High, "Custom build task invoked!");
+            return true;
+        }
+    }
+}
